07c4229ff5ef8454acfc8d15cd24204bf7bd766f
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-07-17 16:57:04 +0600

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
     cors({
       origin: [
-        
+        "https://shoply-client.onrender.com",
         "http://localhost:3000"
       ],
       credentials: true,
